4c7aa23a5710527858c3043c485043ae05aba040
rtbro7 <arturscherbakov@gmail.com>
2025-05-04 10:43:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -36,11 +36,11 @@ async def transcribe_audio(data: AudioRequest):
     try:
         if is_youtube_url(data.url):
             subprocess.run(
-            print("Текущие файлы в директории:", os.listdir("."))
-                ["yt-dlp", "--cookies", "cookies.txt", "-f", "bestaudio", "-o", mp4_path, data.url],
+print("Текущие файлы в директории:", os.listdir("."))
+subprocess.run(
+    ["yt-dlp", "--cookies", "cookies.txt", "-f", "bestaudio", "-o", mp4_path, data.url],
     check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-)
-        else:
+)        else:
             subprocess.run(
                 ["ffmpeg", "-y", "-i", data.url, "-vn", mp4_path],
                 check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
